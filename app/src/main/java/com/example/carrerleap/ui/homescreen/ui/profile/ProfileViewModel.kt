42522f212639c8d3c6b62ddca6d87d121a7c76e6
Dzikriananda <96009309+Dzikriananda@users.noreply.github.com>
2023-06-02 20:58:52 +0700

    rename beberapa file

--- app/src/main/java/com/example/carrerleap/ui/homescreen/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/carrerleap/ui/homescreen/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.carrerleap.ui.homescreen.ui.dashboard
+package com.example.carrerleap.ui.homescreen.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
